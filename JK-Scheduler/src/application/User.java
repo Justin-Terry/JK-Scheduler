f436904f6d714e8c7d28fe750514c86f8c03174e
Kyle Wu <kylewu2@gmail.com>
2018-10-18 17:21:09 -0700

    Added Address to User

--- JK-Scheduler/src/application/User.java
+++ JK-Scheduler/src/application/User.java
@@ -1,11 +1,48 @@
 package application;
 
+import java.util.ArrayList;
+
 public class User {
-	private String username;
-	private String password;
-	private String fName;
-	private String lName;
-	private String phone;
+	private String username,
+			password,
+			fName,
+			lName,
+			phone,
+			userid;
+	private Address address;
+
+	/**
+	 *  Pass an array of information fields from UserController 'createAccount' method
+	 *  to create a new user.
+	 * @param args	- username, password, and personal information
+	 */
+	public User(final ArrayList<String> args) {
+		// Enable assertions option must be enabled for assertions to work
+		final int NUM_ARGS = 9;
+		assert args.size() == NUM_ARGS :
+				String.format("User constructor expected %d arguments, received %d\n", NUM_ARGS, args.size());
+		username = args.get(0);
+		password = args.get(1);
+		fName = args.get(2);
+		lName = args.get(3);
+		phone = args.get(4);
+
+		address = new Address(
+								args.get(5),	// Street
+							  	args.get(6), 	// City
+							  	args.get(7),	// State
+							  	args.get(8)		// Zip
+							 );
+
+		/**
+		 * 	ADD USER ID FUNCTIONALITY
+		 */
+		if (userid == null) {
+			System.out.println("Error: User ID functionality not set.\n");
+			return;
+		}
+	}
+
 	public String getUsername() {
 		return username;
 	}
@@ -36,6 +73,4 @@ public class User {
 	public void setPhone(String phone) {
 		this.phone = phone;
 	}
-	
-	
 }
