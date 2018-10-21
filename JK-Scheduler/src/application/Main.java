3ab0726895f226ffde9a703fa318157d29f166f5
Kyle Wu <kylewu2@gmail.com>
2018-10-20 21:48:09 -0700

    No change

--- JK-Scheduler/src/application/Main.java
+++ JK-Scheduler/src/application/Main.java
@@ -4,8 +4,6 @@ import gui.SettingsWindow;
 import gui.WindowManager;
 import javafx.application.Application;
 import javafx.stage.Stage;
-import javafx.scene.Scene;
-import javafx.scene.layout.BorderPane;
 
 
 public class Main extends Application {
@@ -15,7 +13,6 @@ public class Main extends Application {
 	
 	@Override
 	public void start(Stage primaryStage) {
-		
 		winMan = new WindowManager(primaryStage);
 	}
 	
