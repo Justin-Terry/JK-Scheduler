9818c38920839f12e1b62370c1572bbd9e15f185
Kyle Wu <kylewu2@gmail.com>
2018-12-08 17:19:16 -0800

    reskinned GUI and added notification field

--- JK-Scheduler/src/gui/AppointmentWindow.java
+++ JK-Scheduler/src/gui/AppointmentWindow.java
@@ -15,11 +15,11 @@ import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
 public class AppointmentWindow {
-    protected final Stage stage = new Stage();
-    protected final GridPane gp = new GridPane();
-    protected final Scene scene = new Scene(gp);
-    protected final Button submitButton = new Button("Submit");
-
+    protected Stage stage = new Stage();
+    protected GridPane gp = new GridPane();
+    protected Scene scene = new CustomScene(gp);
+    protected Button submitButton = new CustomButton("Submit", scene);
+    
     protected final String[]
             labels = {"Name", "Type", "Location", "Start", "End"},
             types = {"Private", "Public"},
@@ -53,10 +53,13 @@ public class AppointmentWindow {
             EHB = new ComboBox(hours), // end hr
             EMB = new ComboBox(minutes), // end min
             EPB = new ComboBox(ampmOptions); // end period
+            
+//            AlertBox = new ComboBox(minutes);
 
     protected final TextField
             nameText = new TextField(),
-            locationText = new TextField();
+            locationText = new TextField(),
+            alertText = new TextField();
 
     protected final DatePicker
             dpStart = new DatePicker(),
@@ -67,22 +70,15 @@ public class AppointmentWindow {
             endHolder = new HBox();
 
     public AppointmentWindow() {
-        scene.getStylesheets()
-                .add(
-                        ModifyAppointmentWindow
-                                .class
-                                .getResource("gui.css")
-                                .toExternalForm());
-
-        startHolder.getChildren().addAll(SHB, new Label(" : "), SMB, SPB);
-        endHolder.getChildren().addAll(EHB, new Label(" : "), EMB, EPB);
+        startHolder.getChildren().addAll(SHB,SMB, SPB);
+        endHolder.getChildren().addAll(EHB,EMB, EPB);
 
         /**
          * Add labels, fields, and button(s)
          */
         for(int i = 0; i < labels.length; i++)
             gp.add(new Label(labels[i]), 0, labels[i].equals("End") ? i+1 : i);
-
+        
         gp.add(nameText, 1, 0);
         gp.add(typeOptionBox, 1, 1);
         gp.add(locationText, 1, 2);
@@ -93,8 +89,16 @@ public class AppointmentWindow {
         gp.add(endHolder, 1, 5);
         gp.add(dpEnd, 1, 6);
 
-        gp.add(submitButton, 0, 7);
-        gp.setColumnSpan(submitButton, 2);
+        gp.add(new Label("Alert"), 0, 7);
+        alertText.toFront();
+        gp.add(alertText, 1,7);
+        Label after = new Label("                 minutes before");
+        after.toBack();
+        gp.add(after, 1, 7);
+//        AlertBox.setMinWidth(50.0);
+        
+        gp.add(submitButton, 1, 8);
+//        gp.setColumnSpan(submitButton, 2);
 
         gp.setAlignment(Pos.CENTER);
         gp.setPadding(new Insets(20));
@@ -102,7 +106,8 @@ public class AppointmentWindow {
         gp.setHgap(10);
 
         stage.initModality(Modality.APPLICATION_MODAL);
-        stage.setMinHeight(60*(labels.length+1));
-        stage.setMinWidth(600);
+        stage.sizeToScene();
+//        stage.setMinHeight(60*(labels.length+1));
+//        stage.setMinWidth(600);
     }
 }
